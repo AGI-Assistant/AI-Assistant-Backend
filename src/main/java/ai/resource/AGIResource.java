e4301b4533f2daab79e2379b15f5653bf4d0360e
Ghost <OverlyGenericAddress@pm.me>
2023-08-08 21:46:55 +0200

    Changed to response method

--- src/main/java/ai/resource/AGIResource.java
+++ src/main/java/ai/resource/AGIResource.java
@@ -4,6 +4,8 @@ import ai.service.PromptService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
 import java.util.UUID;
 
 @Path("/ai")
@@ -13,6 +15,20 @@ public class AGIResource {
     PromptService promptService;
 
 
+    // Gets a message by id
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getPrompt(UUID id) {
+        Prompt prompt = promptService.findPromptById(id);
+
+        if (prompt != null) {
+            return Response.ok(prompt).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -21,16 +37,4 @@ public class AGIResource {
         System.out.println(prompt);
         return promptService.addPrompt(prompt);
     }
-
-    @GET
-    @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Prompt getPrompt(UUID id) {
-
-        System.out.println(id);
-        return promptService.findPromptById(id);
-    }
-
-
-
 }
