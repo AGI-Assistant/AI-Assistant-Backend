1fce79730507bf1fb04e635bd9a8bc65b02494f6
Ghost <OverlyGenericAddress@pm.me>
2023-08-08 14:47:29 +0200

    Test spring boot ResponseEntity

--- src/main/java/ai/resource/AGIResource.java
+++ src/main/java/ai/resource/AGIResource.java
@@ -1,12 +1,13 @@
 package ai.resource;
-
 import ai.model.Prompt;
 import ai.service.PromptService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-
 import java.util.UUID;
+
+import jakarta.ws.rs.core.Response;
+import org.http.ResponseEntity;
 
 @Path("/ai")
 public class AGIResource {
@@ -14,23 +15,43 @@ public class AGIResource {
     @Inject
     PromptService promptService;
 
-
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public UUID sendPrompt(Prompt prompt) {
-
-
-        return promptService.addPrompt(prompt);
-    }
-
+    /*
+    // Gets a message by id
     @GET
     @Path("/{id}")
     @Produces(MediaType.TEXT_PLAIN)
-    public Prompt getPrompt(UUID id) {
-        return promptService.findPromptById(id);
+	public ResponseEntity<Prompt> getPrompt(UUID id) {
+		Prompt prompt = promptService.findPromptById(id);
+
+		if (prompt != null) {
+			return new ResponseEntity<Prompt>((prompt), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Prompt>(HttpStatus.NOT_FOUND);
+		}
+	}
+    */
+
+    // Gets a message by id
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getPrompt(UUID id) {
+        Prompt prompt = promptService.findPromptById(id);
+
+        if (prompt != null) {
+            return Response.ok(prompt).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 
 
-
+    // Sends a prompt to the AI
+    @POST
+    @Path("/prompt")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public UUID sendPrompt(Prompt prompt) {
+        return promptService.addPrompt(prompt);
+    }
 }
